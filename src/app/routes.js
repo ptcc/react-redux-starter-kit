0264645490f703b6e07ef245cfc2b857553e3eda
Kos <koshuang@gmail.com>
2016-05-07 10:49:42 +0800

    move routes into each modules

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,7 +1,7 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout/CoreLayout'
-import Home from '../routes/Home'
-import CounterRoute from '../routes/Counter'
+import Home from '../home/routes'
+import CounterRoute from '../counter/routes'
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
