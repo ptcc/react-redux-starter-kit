10b4d25cbc40d3f463a81b7a718d069b7935b2bc
Kos <koshuang@gmail.com>
2016-05-07 10:58:31 +0800

    move components into modules folder

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../../components/Header'
+import Header from '../../app/Header'
 import classes from './CoreLayout.scss'
 import '../../styles/core.scss'
 
