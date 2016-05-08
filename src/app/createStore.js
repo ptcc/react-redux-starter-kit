4419c2a82151f24ee1abd18eff0a53e143557ca7
Kos <koshuang@gmail.com>
2016-05-08 21:14:19 +0800

    improve createStore readability

--- src/app/createStore.js
+++ src/app/createStore.js
@@ -1,35 +1,44 @@
-import { applyMiddleware, compose, createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import createSagaMiddleware from 'redux-saga'
-
-import reducers from './reducers'
+import makeRootReducer from './reducers'
 import sagas from './sagas'
 
 export default (initialState = {}, history) => {
+  // ======================================================
+  // Middleware Configuration
+  // ======================================================
   let middleware = applyMiddleware(
     createSagaMiddleware(sagas),
     thunk,
     routerMiddleware(history)
   )
 
-  // Use DevTools chrome extension in development
+  // ======================================================
+  // Developer Tools
+  // ======================================================
   if (__DEBUG__) {
     const devToolsExtension = window.devToolsExtension
 
     if (typeof devToolsExtension === 'function') {
-      middleware = compose(middleware, devToolsExtension())
+      middleware.push(middleware, devToolsExtension())
     }
   }
 
-  const store = createStore(reducers(), initialState, middleware)
-
+  // ======================================================
+  // Store Instantiation and HMR Setup
+  // ======================================================
+  const store = createStore(
+    makeRootReducer(),
+    initialState,
+    middleware
+  )
   store.asyncReducers = {}
 
   if (module.hot) {
     module.hot.accept('./reducers', () => {
       const reducers = require('./reducers').default
-
       store.replaceReducer(reducers)
     })
   }
