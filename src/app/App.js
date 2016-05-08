3ef4ce711b3e2f77a36de1625882ebfd99adcdd6
Kos <koshuang@gmail.com>
2016-05-08 20:56:26 +0800

    improve readability of entry point

--- src/app/App.js
+++ src/app/App.js
@@ -5,16 +5,22 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import createStore from './createStore'
 import { Provider } from 'react-redux'
 
-// Configure history for react-router
+// ========================================================
+// Browser History Setup
+// ========================================================
 const browserHistory = useRouterHistory(createBrowserHistory)({
   basename: __BASENAME__
 })
 
+// ========================================================
+// Store and History Instantiation
+// ========================================================
 // Create redux store and sync with react-router-redux. We have installed the
 // react-router-redux reducer under the key "router" in src/routes/index.js,
 // so we need to provide a custom `selectLocationState` to inform
 // react-router-redux of its location.
-const store = createStore(window.__INITIAL_STATE__, browserHistory)
+const initialState = window.___INITIAL_STATE__
+const store = createStore(initialState, browserHistory)
 const history = syncHistoryWithStore(browserHistory, store, {
   selectLocationState: (state) => state.router
 })
--- src/main.js
+++ src/main.js
@@ -2,20 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app/App'
 
-const MOUNT_ELEMENT = document.getElementById('root')
-
-let render = () => {
-  ReactDOM.render(App, MOUNT_ELEMENT)
+// ========================================================
+// Developer Tools Setup
+// ========================================================
+if (__DEBUG__) {
+  if (window.devToolsExtension) {
+    window.devToolsExtension.open()
+  }
 }
 
-// Enable HMR and catch runtime errors in RedBox
-// This code is excluded from production bundle
+// ========================================================
+// Render Setup
+// ========================================================
+const MOUNT_NODE = document.getElementById('root')
+let render = () => {
+  ReactDOM.render(App, MOUNT_NODE)
+}
+
+// ========================================================
+// Developer Tools Setup
+// ========================================================
 if (__DEV__ && module.hot) {
   const renderApp = render
   const renderError = (error) => {
     const RedBox = require('redbox-react')
 
-    ReactDOM.render(<RedBox error={error} />, MOUNT_ELEMENT)
+    ReactDOM.render(<RedBox error={error} />, MOUNT_NODE)
   }
   render = () => {
     try {
@@ -27,9 +39,7 @@ if (__DEV__ && module.hot) {
   module.hot.accept(['./app/routes'], () => render())
 }
 
-// Use Redux DevTools chrome extension
-if (__DEBUG__) {
-  if (window.devToolsExtension) window.devToolsExtension.open()
-}
-
+// ========================================================
+// Go!
+// ========================================================
 render()
